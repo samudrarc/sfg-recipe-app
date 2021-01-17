c83340c80e65dbc635b136c9c27630a84d4a0c12
samudrarc <samudra_rc@hotmail.com>
2021-01-16 19:49:11 -0500

    Initial commit

---
+++ src/main/java/guru/sfg/recipe/sfgrecipeapp/SfgRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package guru.sfg.recipe.sfgrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgRecipeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/sfg/recipe/sfgrecipeapp/SfgRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.sfg.recipe.sfgrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
